91415b758f5ac8345e10797e64f6d53f8cb58a45
MiSa-JD <jeongs0427@gmail.com>
2025-11-04 03:42:02 +0900

    added TimeZone default on main

--- src/main/java/com/team8/tai_backend/TaiBackendApplication.java
+++ src/main/java/com/team8/tai_backend/TaiBackendApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing(dateTimeProviderRef = "koreaDateTimeProvider")
 @EnableScheduling
 @SpringBootApplication
 public class TaiBackendApplication {
-
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     SpringApplication.run(TaiBackendApplication.class, args);
   }
 
